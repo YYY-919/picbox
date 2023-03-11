929d94b314bed1c5e2f08beb79fe2b9c494e7270
YYY-919 <YYY-919@users.noreply.github.com>
2023-03-11 10:54:47 +0000

    chore: update config file

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -86,6 +86,7 @@ export default {
     "meme/f8d2eaa3a4d621839bd9af54f21c3d791f4712fd.png",
     "meme/fdd82a9f75681f2adb7c151e0e397b664465f328.png",
     "meme/ffe075ac3c3973ed65a22ece7d9281fe02ffb0f1.png",
+    "meme/miku大受震撼.webp",
     "meme/有意思.webp",
     "meme/法国军礼.webp",
     "meme/照坤镜.webp",
