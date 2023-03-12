2f97d2ff3fb6e265374dbb322d83d8a30cdc6291
YYY <91111268+YYY-919@users.noreply.github.com>
2023-03-12 10:05:47 +0800

    Update index.js

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -119,7 +119,7 @@ function view() {
 async function initgallery() {
     document.getElementById(
         'description'
-    ).innerHTML = `NoneBot 群大佬们的日常，目前已有 ${items.length} 张。`
+    ).innerHTML = `一个简简单单的梗图&表情包收集，目前已有 ${items.length} 张`
     document.getElementById('refresh-btn').onclick = () => {
         location.hash = `#${
             items[random(items.length - 1, 0)].match(
